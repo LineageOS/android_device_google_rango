2d9a2e1a2cbad915a427b248b184a4a7dbb75c09
Michael Bestas <mkbestas@lineageos.org>
2026-06-24 10:32:51 +0300

    rango: Update properties from CP2A.260605.012
    Change-Id: Id3b703fb13a9418d3e9b1c61efab6d5154a66f6a

--- aosp_rango.mk
+++ aosp_rango.mk
@@ -8,6 +8,12 @@ $(call inherit-product, device/google/rango/device-rango.mk)
 
 PRODUCT_NAME := aosp_rango
 PRODUCT_DEVICE := rango
-PRODUCT_MODEL := AOSP on rango
-PRODUCT_BRAND := Android
+PRODUCT_MODEL := Pixel 10 Pro Fold
+PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
+
+PRODUCT_NAME_FOR_ATTESTATION := rango
+PRODUCT_DEVICE_FOR_ATTESTATION := rango
+PRODUCT_MODEL_FOR_ATTESTATION := Pixel 10 Pro Fold
+PRODUCT_BRAND_FOR_ATTESTATION := google
+PRODUCT_MANUFACTURER_FOR_ATTESTATION := Google
